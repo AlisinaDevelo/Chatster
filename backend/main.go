18510e8228bb844b167f98a2ab298edfd5a0a574
AliSina Karimi <100498538+AliSinaDevelo@users.noreply.github.com>
2024-09-28 19:49:10 +0200

    endpoint converter from http to ws

--- backend/main.go
+++ backend/main.go
@@ -2,9 +2,54 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
+
+// will need a Read and Write buffer size
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+
+	// will need to check the origin of the request
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+// define a reader which will listen for
+// new messages being sent to our WebSocket endpoint
+func reader(conn *websocket.Conn) {
+	for {
+		// read in a message
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		// print out that message for clarity
+		fmt.Println(string(p))
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+func serveWs(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Host)
+
+	// upgrades connection to a websocket connection
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	reader(ws)
+}
 
 func setupRoutes() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -15,5 +60,5 @@ func setupRoutes() {
 func main() {
 	setupRoutes()
 	http.ListenAndServe(":8080", nil)
-	// fmt.Println("App v0.01")
+	fmt.Println("App v0.01")
 }
