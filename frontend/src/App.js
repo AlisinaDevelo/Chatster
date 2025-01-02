a19392568fc93f72f5ea5292672f53750676d8e4
AliSinaDevelo <alisinakarimi.2003@gmail.com>
2025-01-02 19:49:10 +0100

    main component breakup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,59 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import { connect, sendMsg } from './api';
 import Header from './components/Header/Header';
+import ChatHistory from './components/ChatHistory/ChatHistory';
+import ChatInput from './components/ChatInput/ChatInput';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    connect();
-  }
+function App() {
+  const [chatHistory, setChatHistory] = useState([]);
+  const [hasUsername, setHasUsername] = useState(false);
 
-  send() {
-    console.log('Clicked');
-    sendMsg('Hello Server!');
-  }
+  useEffect(() => {
+    connect((msg) => {
+      const parsedMessage = JSON.parse(msg.data);
+      setChatHistory(prevChatHistory => [...prevChatHistory, parsedMessage]);
+    });
+  }, []);
 
-  render() {
-    return (
-      <div className="App">
-        <button onClick={this.send}>Send Message</button>
+  const send = (message) => {
+    if (!hasUsername) {
+      // If this is the first message, it's setting a username
+      sendMsg(JSON.stringify({
+        type: 'username',
+        content: message
+      }));
+      setHasUsername(true);
+      
+      // Add local message to show username set
+      setChatHistory(prev => [
+        ...prev, 
+        {
+          type: 'notification',
+          username: 'System',
+          content: `You joined as ${message}`
+        }
+      ]);
+    } else {
+      // Regular chat message
+      sendMsg(JSON.stringify({
+        type: 'message',
+        content: message
+      }));
+    }
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      <div className="chat-container">
+        <ChatHistory chatHistory={chatHistory} />
+        <ChatInput sendMessage={send} hasUsername={hasUsername} />
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-
 
 /* function App() {
   return (
