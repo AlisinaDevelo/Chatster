518962040b045884af980f50fcd92b8242891596
AliSinaDevelo <alisinakarimi.2003@gmail.com>
2025-01-03 19:59:10 +0100

    callback

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,6 @@
 var socket = new WebSocket('ws://localhost:8080/ws');
 
-let connect = () => {
+let connect = (cb) => {
   console.log("Attempting Connection..");
 
   socket.onopen = () => {
@@ -8,7 +8,8 @@ let connect = () => {
   };
 
   socket.onmessage = (msg) => {
-    console.log(msg);
+    console.log("Received message:", msg);
+    cb(msg);
   };
 
   socket.onclose = (event) => {
